2902634c4c4a3b99cdb2f80683e51681a24a52c7
Paolo Bellini <paolobellini.web@gmail.com>
2026-04-20 15:49:55 +0200

    feat: publish italian translations

---
+++ lang/it/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'failed' => 'Queste credenziali non corrispondono ai nostri record.',
+    'password' => 'La password fornita non è corretta.',
+    'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
+
+];
