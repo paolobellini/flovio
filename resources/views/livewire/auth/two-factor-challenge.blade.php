03b348aac2dabc4f6306d2a20c3d1d8b78312e3b
Paolo Bellini <paolobellini.web@gmail.com>
2026-04-20 16:29:59 +0200

    feat: restyle two factor challenge page (flv-5)

--- resources/views/livewire/auth/two-factor-challenge.blade.php
+++ resources/views/livewire/auth/two-factor-challenge.blade.php
@@ -1,5 +1,5 @@
-<x-layouts::auth :title="__('Two-factor authentication')">
-    <div class="flex flex-col gap-6">
+<x-layouts::auth.split :title="__('Two-factor authentication')">
+    <div class="flex flex-col gap-8">
         <div
             class="relative w-full h-auto"
             x-cloak
@@ -24,25 +24,25 @@
             }"
         >
             <div x-show="!showRecoveryInput">
-                <x-auth-header
-                    :title="__('Authentication code')"
-                    :description="__('Enter the authentication code provided by your authenticator application.')"
-                />
+                <div class="flex flex-col gap-1 text-center">
+                    <h1 class="text-2xl font-bold tracking-tight text-zinc-900">{{ __('Authentication code') }}</h1>
+                    <p class="text-sm text-zinc-500">{{ __('Enter the authentication code provided by your authenticator application.') }}</p>
+                </div>
             </div>
 
             <div x-show="showRecoveryInput">
-                <x-auth-header
-                    :title="__('Recovery code')"
-                    :description="__('Please confirm access to your account by entering one of your emergency recovery codes.')"
-                />
+                <div class="flex flex-col gap-1 text-center">
+                    <h1 class="text-2xl font-bold tracking-tight text-zinc-900">{{ __('Recovery code') }}</h1>
+                    <p class="text-sm text-zinc-500">{{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}</p>
+                </div>
             </div>
 
             <form method="POST" action="{{ route('two-factor.login.store') }}">
                 @csrf
 
-                <div class="space-y-5 text-center">
+                <div class="mt-8 space-y-5 text-center">
                     <div x-show="!showRecoveryInput">
-                        <div class="flex items-center justify-center my-5">
+                        <div class="flex items-center justify-center">
                             <flux:otp
                                 x-model="code"
                                 length="6"
@@ -55,41 +55,33 @@
                     </div>
 
                     <div x-show="showRecoveryInput">
-                        <div class="my-5">
-                            <flux:input
-                                type="text"
-                                name="recovery_code"
-                                x-ref="recovery_code"
-                                x-bind:required="showRecoveryInput"
-                                autocomplete="one-time-code"
-                                x-model="recovery_code"
-                            />
-                        </div>
+                        <flux:input
+                            type="text"
+                            name="recovery_code"
+                            x-ref="recovery_code"
+                            x-bind:required="showRecoveryInput"
+                            autocomplete="one-time-code"
+                            x-model="recovery_code"
+                        />
 
                         @error('recovery_code')
-                            <flux:text color="red">
+                            <flux:text color="red" class="mt-2">
                                 {{ $message }}
                             </flux:text>
                         @enderror
                     </div>
 
-                    <flux:button
-                        variant="primary"
-                        type="submit"
-                        class="w-full"
-                    >
+                    <flux:button variant="primary" type="submit" class="w-full">
                         {{ __('Continue') }}
                     </flux:button>
                 </div>
 
-                <div class="mt-5 space-x-0.5 text-sm leading-5 text-center">
-                    <span class="opacity-50">{{ __('or you can') }}</span>
-                    <div class="inline font-medium underline cursor-pointer opacity-80">
-                        <span x-show="!showRecoveryInput" @click="toggleInput()">{{ __('login using a recovery code') }}</span>
-                        <span x-show="showRecoveryInput" @click="toggleInput()">{{ __('login using an authentication code') }}</span>
-                    </div>
+                <div class="mt-5 space-x-0.5 text-sm leading-5 text-center text-zinc-500">
+                    <span>{{ __('or you can') }}</span>
+                    <span class="inline font-medium underline cursor-pointer text-zinc-700" x-show="!showRecoveryInput" @click="toggleInput()">{{ __('login using a recovery code') }}</span>
+                    <span class="inline font-medium underline cursor-pointer text-zinc-700" x-show="showRecoveryInput" @click="toggleInput()">{{ __('login using an authentication code') }}</span>
                 </div>
             </form>
         </div>
     </div>
-</x-layouts::auth>
+</x-layouts::auth.split>
